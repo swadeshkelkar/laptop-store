41369e6e443f9fd02f825a2dec83f8f2bc5e0dde
Swadesh Kelkar <swadeshkelkar07@gmail.com>
2021-01-26 11:41:26 +0530

    Changing port for deployment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const port = process.env.PORT || 3000;
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
 const laptopData = JSON.parse(json);
@@ -58,7 +59,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(1337, "127.0.0.1", () => {
+server.listen(port, () => {
   console.log("Listening for requests now!");
 });
 
